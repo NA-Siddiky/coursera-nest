fd6f8d0d41ce24ac5ba498710eaa7a5c0039268c
NA-Siddiky <siddiky.academic@gmail.com>
2025-09-01 10:25:47 +0000

    refactor: reorganize imports and enhance response handling in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AnswerDto } from './dto/app.dto';
 
@@ -11,10 +20,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get('/response')
+  getResponse(@Req() req, @Res() res) {
+    // console.log(req.headers);
+    // console.log(res);
+    // return res.send('Hello Nest');
+    res.status(200).json({ res: 'Hello Nest' });
+  }
+
   @Get('/userName')
   getQueryString(
     @Query('name') userName: string,
-    @Query('age') userAge: number
+    @Query('age') userAge: number,
   ): string {
     console.log('Received User Name:', userName);
     console.log('Received User Age:', userAge);
@@ -23,13 +40,29 @@ export class AppController {
 
   @Get('/askQuestion')
   askQuestion(): string {
-    return "How are you?";
+    return 'How are you?';
   }
- 
+
+  // @Post('/answer')
+  // answer(@Body() getAnswerDto: AnswerDto): string {
+  //   console.log('Received answer:', getAnswerDto.answer);
+  //   return `Answer: ${getAnswerDto.answer}`;
+  // }
+
   @Post('/answer')
-  answer(@Body() getAnswerDto: AnswerDto): string {
-    console.log('Received answer:', getAnswerDto.answer);
-    return `Answer: ${getAnswerDto.answer}`;
+  answer(@Body() getAnswerDto: AnswerDto, @Req() req, @Res() res) {
+    let response;
+    let status;
+    if (req.body.answer === 'yes') {
+      response = `You answered yes!`;
+      status = 200;
+    } else {
+      response = `You answered no!`;
+      status = 400;
+    }
+    res.status(status).json({
+      res: response,
+    });
   }
 
   @Get(':id')
@@ -37,5 +70,4 @@ export class AppController {
     console.log('Received User ID:', userId);
     return `User ID: ${userId}`;
   }
-
 }
