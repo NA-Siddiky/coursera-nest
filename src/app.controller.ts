b1cbb31e60c43ff6a8e616b227feaf0c3a38b424
NA-Siddiky <siddiky.academic@gmail.com>
2025-09-01 09:48:43 +0000

    feat: add user query handling and answer DTO for improved functionality

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnswerDto } from './dto/app.dto';
 
 @Controller()
 export class AppController {
@@ -9,4 +10,32 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/userName')
+  getQueryString(
+    @Query('name') userName: string,
+    @Query('age') userAge: number
+  ): string {
+    console.log('Received User Name:', userName);
+    console.log('Received User Age:', userAge);
+    return `Hello, ${userName}! You are ${userAge} years old.`;
+  }
+
+  @Get('/askQuestion')
+  askQuestion(): string {
+    return "How are you?";
+  }
+ 
+  @Post('/answer')
+  answer(@Body() getAnswerDto: AnswerDto): string {
+    console.log('Received answer:', getAnswerDto.answer);
+    return `Answer: ${getAnswerDto.answer}`;
+  }
+
+  @Get(':id')
+  getRouteParam(@Param('id') userId: string): string {
+    console.log('Received User ID:', userId);
+    return `User ID: ${userId}`;
+  }
+
 }
